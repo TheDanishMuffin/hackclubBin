893779027876bb1cfedf448d46ebcc002c86d7f1
Dinesh Babu <60652261+TheDanishMuffin@users.noreply.github.com>
2024-07-30 19:31:57 -0400

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,49 +1,110 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_SH110X.h>
-#include <LiquidCrystal.h>
+#include <Adafruit_SSD1306.h>
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET    -1
 
-Adafruit_SH1107 display = Adafruit_SH1107(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 const int joystickVert = 26;
 const int joystickHorz = 27;
 const int joystickSel = 28;
 
-const int buzzerPin = 10;
+const int center = 511;
+const int threshold = 200;
 
-int score = 0;
-int highScore = 0;
+#define GRID_SIZE 4
+#define MAX_SNAKE_LENGTH 128
+int snakeX[MAX_SNAKE_LENGTH];
+int snakeY[MAX_SNAKE_LENGTH];
+int snakeLength = 5;
+int foodX, foodY;
+int direction = 3;  // 0 = up, 1 = dwn, 2 = LLeft, 3 = right
 
 void setup() {
   Serial1.begin(115200);
   Serial1.println("Hello, Raspberry Pi Pico W!");
 
-  if(!display.begin(0x3C, OLED_RESET)) {
-    Serial1.println(F("SH1107 allocation failed"));
-    for(;;);
+  if (!display.begin(SSD1306_I2C_ADDRESS, OLED_RESET)) {
+    Serial1.println(F("SSD1306 allocation failed"));
+    for (;;);
   }
   display.display();
   delay(2000);
   display.clearDisplay();
   display.setTextSize(1);
-  display.setTextColor(SH110X_WHITE);
-
-  lcd.begin(16, 2);
-  lcd.print("Welcome!");
+  display.setTextColor(SSD1306_WHITE);
 
   pinMode(joystickVert, INPUT);
   pinMode(joystickHorz, INPUT);
   pinMode(joystickSel, INPUT_PULLUP);
 
-  pinMode(buzzerPin, OUTPUT);
+  for (int i = 0; i < snakeLength; i++) {
+    snakeX[i] = snakeLength - i - 1; 
+    snakeY[i] = 0;                   
+  }
+
+  generateFood();
 }
 
 void loop() {
-  display.clearDisplay();
-  display.setCursor(0,0);
+  int vert = analogRead(joystickVert);
+  int horz = analogRead(joystickHorz);
+  int sel = digitalRead(joystickSel);
+
+  if (vert > center + threshold && direction != 1) {
+    direction = 0;  // Up
+  } else if (vert < center - threshold && direction != 0) {
+    direction = 1;  // Down
+  } else if (horz > center + threshold && direction != 2) {
+    direction = 3;  // Right
+  } else if (horz < center - threshold && direction != 3) {
+    direction = 2;  // Left
+  }
+  
+  updateSnake();
+
+  if (checkCollision()) {
+    gameOver();
+  }
+
+  if (snakeX[0] == foodX && snakeY[0] == foodY) {
+    if (snakeLength < MAX_SNAKE_LENGTH) {
+      snakeLength++;
+    }
+    generateFood();
+  }
+
+  displayGame();
+
+  delay(100);
+}
+
+void updateSnake() {
+  for (int i = snakeLength - 1; i > 0; i--) {
+    snakeX[i] = snakeX[i - 1];
+    snakeY[i] = snakeY[i - 1];
+  }
+
+  if (direction == 0) {
+    snakeY[0]--;
+  } else if (direction == 1) {
+    snakeY[0]++;
+  } else if (direction == 2) {
+    snakeX[0]--;
+  } else if (direction == 3) {
+    snakeX[0]++;
+  }
+
+  if (snakeX[0] < 0) snakeX[0] = SCREEN_WIDTH / GRID_SIZE - 1;
+  if (snakeX[0] >= SCREEN_WIDTH / GRID_SIZE) snakeX[0] = 0;
+  if (snakeY[0] < 0) snakeY[0] = SCREEN_HEIGHT / GRID_SIZE - 1;
+  if (snakeY[0] >= SCREEN_HEIGHT / GRID_SIZE) snakeY[0] = 0;
+}
+
+bool checkCollision() {
+  for (int i = 1; i < snakeLength; i++) {
+    if (snakeX[0] == snakeX[i] && snakeY[0] == snakeY[i]) {
+      return true;
