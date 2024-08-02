0ce4002ec8ebf0b9cf11846d9008f3ada49cd49b
Dinesh Babu <60652261+TheDanishMuffin@users.noreply.github.com>
2024-08-01 20:29:53 -0400

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -4,7 +4,7 @@
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-#define OLED_RESET -1
+#define OLED_RESET    -1
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
@@ -23,20 +23,7 @@ int snakeLength = 5;
 int foodX, foodY;
 int direction = 3;
 
-unsigned long lastMoveTime = 0;
-unsigned long moveInterval = 150;
 int score = 0;
-int highScore = 0;
-bool isPaused = false;
-bool gameOverFlag = false;
-bool gameStarted = false;
-int obstacleX[10], obstacleY[10];
-int numObstacles = 5;
-
-int powerUpX, powerUpY;
-bool powerUpActive = false;
-unsigned long lastPowerUpTime = 0;
-unsigned long powerUpDuration = 10000;
 
 void setup() {
   Serial1.begin(115200);
@@ -46,6 +33,7 @@ void setup() {
     Serial1.println(F("SSD1306 allocation failed"));
     for (;;);
   }
+
   display.display();
   delay(2000);
   display.clearDisplay();
@@ -56,50 +44,10 @@ void setup() {
   pinMode(joystickHorz, INPUT);
   pinMode(joystickSel, INPUT_PULLUP);
 
-  resetGame();
-  showStartScreen();
+  startGame();
 }
 
 void loop() {
-  if (!gameOverFlag) {
-    unsigned long currentTime = millis();
-    if (currentTime - lastMoveTime > moveInterval && !isPaused) {
-      lastMoveTime = currentTime;
-
-      if (!gameStarted) {
-        if (digitalRead(joystickSel) == LOW) {
-          gameStarted = true;
-          display.clearDisplay();
-          display.display();
-          delay(500);
-        }
-      } else {
-        handleJoystickInput();
-        if (!isPaused) {
-          updateSnake();
-          if (checkCollision() || checkObstacleCollision()) {
-            gameOver();
-          }
-          if (snakeX[0] == foodX && snakeY[0] == foodY) {
-            handleFoodConsumption();
-          }
-          if (powerUpActive && currentTime - lastPowerUpTime > powerUpDuration) {
-            powerUpActive = false;
-            increaseDifficulty();
-          }
-          if (snakeX[0] == powerUpX && snakeY[0] == powerUpY) {
-            handlePowerUp();
-          }
-          displayGame();
-        } else {
-          pauseGame();
-        }
-      }
-    }
-  }
-}
-
-void handleJoystickInput() {
   int vert = analogRead(joystickVert);
   int horz = analogRead(joystickHorz);
   int sel = digitalRead(joystickSel);
@@ -109,15 +57,28 @@ void handleJoystickInput() {
   } else if (vert < center - threshold && direction != 0) {
     direction = 1;
   } else if (horz > center + threshold && direction != 2) {
-    direction = 3;
-  } else if (horz < center - threshold && direction != 3) {
     direction = 2;
+  } else if (horz < center - threshold && direction != 3) {
+    direction = 3;
   }
 
-  if (sel == LOW) {
-    isPaused = !isPaused;
-    delay(300);
+  updateSnake();
+
+  if (checkCollision()) {
+    gameOver();
   }
+
+  if (snakeX[0] == foodX && snakeY[0] == foodY) {
+    if (snakeLength < MAX_SNAKE_LENGTH) {
+      snakeLength++;
+    }
+    score++;
+    generateFood();
+  }
+
+  displayGame();
+
+  delay(100);
 }
 
 void updateSnake() {
@@ -136,14 +97,9 @@ void updateSnake() {
     snakeX[0]++;
   }
 
-  wrapAroundScreen();
-}
-
-void wrapAroundScreen() {
-  if (snakeX[0] < 0) snakeX[0] = SCREEN_WIDTH / GRID_SIZE - 1;
-  if (snakeX[0] >= SCREEN_WIDTH / GRID_SIZE) snakeX[0] = 0;
-  if (snakeY[0] < 0) snakeY[0] = SCREEN_HEIGHT / GRID_SIZE - 1;
-  if (snakeY[0] >= SCREEN_HEIGHT / GRID_SIZE) snakeY[0] = 0;
+  if (snakeX[0] < 0 || snakeX[0] >= SCREEN_WIDTH / GRID_SIZE || snakeY[0] < 0 || snakeY[0] >= SCREEN_HEIGHT / GRID_SIZE) {
+    gameOver();
+  }
 }
 
 bool checkCollision() {
@@ -155,44 +111,37 @@ bool checkCollision() {
   return false;
 }
 
-bool checkObstacleCollision() {
-  for (int i = 0; i < numObstacles; i++) {
-    if (snakeX[0] == obstacleX[i] && snakeY[0] == obstacleY[i]) {
-      return true;
-    }
-  }
-  return false;
-}
-
 void gameOver() {
-  gameOverFlag = true;
   display.clearDisplay();
   display.setCursor(0, 0);
-  display.print("Game Over");
+  display.print("Game Over :()");
   display.setCursor(0, 10);
   display.print("Score: ");
   display.print(score);
   display.setCursor(0, 20);
-  display.print("High Score: ");
-  display.print(highScore);
+  display.print("Press SEL to Restart");
   display.display();
-  delay(3000);
-  resetGame();
+
+  while (true) {
+    if (digitalRead(joystickSel) == LOW) {
+      startGame();
+      break;
+    }
+  }
 }
 
-void handleFoodConsumption() {
-  if (snakeLength < MAX_SNAKE_LENGTH) {
-    snakeLength++;
+void startGame() {
+  snakeLength = 5;
+  direction = 3;
+  score = 0;
+
+  for (int i = 0; i < snakeLength; i++) {
+    snakeX[i] = snakeLength - i - 1;
+    snakeY[i] = 0;
   }
+
   generateFood();
-  score += 10;
-  if (score > highScore) {
-    highScore = score;
-  }
-  increaseDifficulty();
-  if (random(0, 100) < 10) {
-    generatePowerUp();
-  }
+  display.clearDisplay();
 }
 
 void generateFood() {
@@ -207,121 +156,17 @@ void generateFood() {
         break;
       }
     }
-    for (int i = 0; i < numObstacles; i++) {
-      if (obstacleX[i] == foodX && obstacleY[i] == foodY) {
-        validPosition = false;
-        break;
-      }
-    }
-  }
-}
-
-void generateObstacles() {
-  for (int i = 0; i < numObstacles; i++) {
-    bool validPosition = false;
-    while (!validPosition) {
-      validPosition = true;
-      obstacleX[i] = random(0, SCREEN_WIDTH / GRID_SIZE);
-      obstacleY[i] = random(0, SCREEN_HEIGHT / GRID_SIZE);
-      for (int j = 0; j < snakeLength; j++) {
-        if (snakeX[j] == obstacleX[i] && snakeY[j] == obstacleY[i]) {
-          validPosition = false;
-          break;
-        }
-      }
-      if (foodX == obstacleX[i] && foodY == obstacleY[i]) {
-        validPosition = false;
-      }
-    }
-  }
-}
-
-void generatePowerUp() {
-  bool validPosition = false;
-  while (!validPosition) {
-    validPosition = true;
-    powerUpX = random(0, SCREEN_WIDTH / GRID_SIZE);
-    powerUpY = random(0, SCREEN_HEIGHT / GRID_SIZE);
-    if (powerUpX != foodX || powerUpY != foodY) {
-      for (int i = 0; i < snakeLength; i++) {
-        if (snakeX[i] == powerUpX && snakeY[i] == powerUpY) {
-          validPosition = false;
-          break;
-        }
-      }
-      for (int i = 0; i < numObstacles; i++) {
-        if (obstacleX[i] == powerUpX && obstacleY[i] == powerUpY) {
-          validPosition = false;
-          break;
-        }
-      }
-      powerUpActive = true;
-      lastPowerUpTime = millis();
-    }
-  }
-}
-
-void handlePowerUp() {
-  if (powerUpActive) {
-    score += 50;
-    powerUpActive = false;
-    increaseDifficulty();
   }
 }
 
 void displayGame() {
   display.clearDisplay();
+
   display.fillRect(foodX * GRID_SIZE, foodY * GRID_SIZE, GRID_SIZE, GRID_SIZE, SSD1306_WHITE);
+
   for (int i = 0; i < snakeLength; i++) {
     display.fillRect(snakeX[i] * GRID_SIZE, snakeY[i] * GRID_SIZE, GRID_SIZE, GRID_SIZE, SSD1306_WHITE);
   }
-  for (int i = 0; i < numObstacles; i++) {
-    display.drawRect(obstacleX[i] * GRID_SIZE, obstacleY[i] * GRID_SIZE, GRID_SIZE, GRID_SIZE, SSD1306_WHITE);
-  }
-  if (powerUpActive) {
-    display.fillRect(powerUpX * GRID_SIZE, powerUpY * GRID_SIZE, GRID_SIZE, GRID_SIZE, SSD1306_WHITE);
-  }
-  display.setCursor(0, 0);
-  display.print("Score: ");
-  display.print(score);
-  display.setCursor(0, 10);
-  display.print("High Score: ");
-  display.print(highScore);
+
   display.display();
-}
-
-void pauseGame() {
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.print("Paused");
-  display.display();
-}
-
-void resetGame() {
-  snakeLength = 5;
-  score = 0;
-  generateFood();
-  generateObstacles();
-  powerUpActive = false;
-  gameOverFlag = false;
-  isPaused = false;
-  gameStarted = false;
-  displayGame();
-}
-
-void showStartScreen() {
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.print("Snake Game");
-  display.setCursor(0, 10);
-  display.print("Press Button");
-  display.display();
-}
-
-void increaseDifficulty() {
-  if (score > 0 && score % 50 == 0) {
-    if (moveInterval > 50) {
-      moveInterval -= 10;
-    }
-  }
 }
