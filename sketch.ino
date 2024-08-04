9815d96dfcee94ae2ed704c82b550fecb130ec50
Dinesh Babu <60652261+TheDanishMuffin@users.noreply.github.com>
2024-08-04 09:08:40 -0400

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -603,3 +603,59 @@ void selectGameMode() {
 
   int selectedOption = 0;
   bool selectionMade = false;
+  while (!selectionMade) {
+    int vert = analogRead(joystickVert);
+    if (vert < center - threshold) {
+      selectedOption = 1;
+    } else if (vert > center + threshold) {
+      selectedOption = 2;
+    } else {
+      selectedOption = 0;
+    }
+
+    display.setCursor(0, 10);
+    if (selectedOption == 0) {
+      display.print("> Normal");
+    } else {
+      display.print("  Normal");
+    }
+
+    display.setCursor(0, 20);
+    if (selectedOption == 1) {
+      display.print("> Growth Multiplier");
+    } else {
+      display.print("  Growth Multiplier");
+    }
+
+    display.display();
+
+    if (digitalRead(joystickSel) == LOW) {
+      while (digitalRead(joystickSel) == LOW);
+      if (selectedOption == 0) {
+        selectedGameMode = NORMAL;
+        growthMultiplier = 1;
+      } else if (selectedOption == 1) {
+        selectedGameMode = GROWTH_MULTIPLIER;
+        growthMultiplier = 2;
+      }
+      selectionMade = true;
+    }
+  }
+
+  delay(1000);
+}
+
+
+void selectGameSpeed() {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print("Select Game Speed:");
+
+  int selectedOption = 1;
+  bool selectionMade = false;
+  while (!selectionMade) {
+    int vert = analogRead(joystickVert);
+    if (vert < center - threshold) {
+      selectedOption = 0;
+    } else if (vert > center + threshold) {
+      selectedOption = 2;
